e60f28fb01c092e4d7d7bfb7963a5d2a690eea5e
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2022-11-07 11:40:04 +0700

    fix

---
+++ work1/src/pages/index.vue
@@ -0,0 +1,48 @@
+<!--
+<template>
+  <div>
+    <h1>ชื่อผู้สมัครงาน :{{ firstname }}</h1>
+    <h1>นามสกุล :{{ lastname }}</h1>
+    <h1>อายุ :{{ age }}</h1>
+    <h1>ที่อยู่ : <span v-html="address"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstname : "Wuttichay",
+      lastname : "Polkunthod",
+      age : 19,
+      address : "<i>Chonburi</i>",
+    };
+  },
+};
+</script>
+-->
+<template>
+  <div>
+    <h1>ชื่อ - นามสกุลผู้สมัครงาน :{{ getFullname() }}</h1>
+    <h1>อายุ :{{ age }}</h1>
+    <h1>ที่อยู่ : <span v-html="address"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      firstname : "Wuttichay" ,
+      lastname: "Polkunthod" ,
+      age: 19 ,
+      address: "<i>Chonburi</i>",
+    };
+  },
+  methods: {
+    getFullname(){
+      return`${this.firstname} ` + `${this.lastname}`
+    }
+  }
+};
+</script>
