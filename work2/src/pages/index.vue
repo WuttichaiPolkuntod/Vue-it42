28b36ac6b483716f1c685ff27c2a13edf7ff9261
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2022-11-21 10:46:33 +0700

    work4

--- work2/src/pages/index.vue
+++ work2/src/pages/index.vue
@@ -1,60 +1,10 @@
 <!--
 <template>
-    <div>
-    <form action="">
-      <div class="">
-      Sign-in
-      </div>
-      <div>
-        <label for="username">Username</label>
-        <input type="text" id="username" value="user1">
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" value="pass1">
-      </div>
-      <div>
-        <input for="checkbox" id="remember" checked>
-        <label for="remember">Remember for username</label>
-      </div>
-      <div>
-        <button type="button" disabled>Sign-in</button>
-        <button type="reset">Reset</button>
-      </div>
-    </form>
-  </div>
-</template>
-
--->
-<template>
   <div>
-  <img :src="picture" :width="size" :height="size"><br>
-  <form @submit="submitForm">
-    <label for=""></label>
-    <input type="text" @input="setNickName">
-    <button type="submit">บันทึก</button>
-  </form>
-    <p>ชื่อ : {{ firstname }}</p>
-    <p>นามสกุล : {{ lastname }}</p>
-    <p>อายุ :{{ age }}</p>
-    <p>ที่อยู่ : <span v-html="address"></span></p>
-    <p>งานอดิเรก : </p>
-      <ul>
-        <li>{{ hobby[0] }}</li>
-        <li>{{ hobby[1] }}</li>
-        <li>{{ hobby[2] }}</li>
-      </ul>
-    <p>ข้อมูลพื้นฐาน : </p>
-      <ul>
-        <li>เพศ : {{ general.gender }}</li>
-        <li>น้ำหนัก : {{ general.weight }}</li>
-        <li>ส่วนสูง : {{ general.height }}</li>
-        <li>โรคประจำตัว : {{ general.status }}</li>
-        </ul>
-        <button @click="showData">คลิกเพื่อดูข้อมูล</button>
-        <button @click="increment(10)">เพิ่มคำ</button>
-        <button @click="decrement(3)">ลดค่า</button>
-
+    <h1>ชื่อผู้สมัครงาน :{{ firstname }}</h1>
+    <h1>นามสกุล :{{ lastname }}</h1>
+    <h1>อายุ :{{ age }}</h1>
+    <h1>ที่อยู่ : <span v-html="address"></span></h1>
   </div>
 </template>
 
@@ -66,31 +16,33 @@ export default {
       lastname : "Polkunthod",
       age : 19,
       address : "<i>Chonburi</i>",
-      picture : "https://cdn-icons-png.flaticon.com/512/644/644617.png",
-      size:150,
-      hobby : ["ทำสวน","เล่นเกม","ดูหนัง"],
-      general : {gender: "male",weight : 60,height : 180,status : false}
     };
   },
-  methods : {
-      getFullname(){
-        return $(this.firstname)      },
-      showData(){
-        alert(this.firstname)
-        },
-        increment(value){
-          this.age+= value
-        },
-        decrement(value){
-          this.age -= value
-        },
-        setNickName(event){
-          this.nickname=event.target.value
-        },
-        submitForm(e){
-          e.preventDefault();
-          alert('บันทึกข้อมูลเรียบร้อย')
-        }
-      }
+};
+</script>
+-->
+<template>
+  <div>
+    <h1>ชื่อ - นามสกุลผู้สมัครงาน :{{ getFullname() }}</h1>
+    <h1>อายุ :{{ age }}</h1>
+    <h1>ที่อยู่ : <span v-html="address"></span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      firstname : "Wuttichay" ,
+      lastname: "Polkunthod" ,
+      age: 19 ,
+      address: "<i>Chonburi</i>",
+    };
+  },
+  methods: {
+    getFullname(){
+      return`${this.firstname} ` + `${this.lastname}`
+    }
+  }
 };
 </script>
---
+++ work4/src/pages/signin.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+  <form action="">
+    <div class="">
+    Sign-in
+    </div>
+    <div class="input">
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="username">{{username}}
+    </div>
+    <div class="input">
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password">{{password}}
+    </div>
+    <div class="input">
+      <input type="checkbox" id="remember" v-model="remember">
+      <label for="remember">Remember for username</label>
+    </div>
+    <div class="input">
+      <button type="button" :disabled="lock">Sign-in</button>
+      <button type="reset" class="">Reset</button>
+    </div>
+  </form>
+</div>
+</template>
+<script>
+ export default {
+  data(){
+    return{
+      username:"",
+      password:"",
+      remember:true,
+      //lock:true
+    }
+  },
+  // created(){
+  //   console.log('created')
+  //   let n = 1
+  //   setInterval(()=>{
+  //     this.username ='test' + n
+  //     n++
+  //     console.log("ok")
+  // },1000)
+  // },
+  computed:{
+    lock(){
+      return this.username === '' || this.password ===''
+    }
+  },
+
+ }
+</script>
+<style scoped>
+  .container{
+    width: 500px;
+    margin: 100px auto;
+    border: 1px solid #000;
+    padding: 10px;
+    background-color: pink;
+  }
+  .input{
+    padding: 5px;
+    margin: 5px;
+  }
+</style>
