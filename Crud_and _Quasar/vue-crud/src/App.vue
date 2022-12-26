9e154cc6cdb4c507fd63472e1cbc424b293b4512
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2022-12-26 09:59:27 +0700

    Work7

---
+++ Crud_and _Quasar/vue-crud/src/App.vue
@@ -0,0 +1,30 @@
+<script>
+  import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/about" label="About" />
+        <q-route-tab to="/create" label="Create" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
---
+++ Crud_and _Quasar/vue-crud/src/views/CreateView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px">
+    <q-form 
+    @submit="onSubmit"
+    class="q-gutter-md"
+    >
+      <q-input v-model="fname" label="First Name" />
+      <q-input v-model="lname" label="Last Name" />
+      <q-input v-model="Username" label="Username" />
+      <q-input v-model="Password" label="Password" />
+      <q-input v-model="Email" label="Email" />
+      <q-input v-model="Avatar" label="Avatar" />
+      <q-btn label="Submit" type="submit" color="primary" />
+    </q-form>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const fname = ref('')
+  const lname = ref('')
+  const Username = ref('')
+  const Password = ref('')
+  const Email = ref('')
+  const Avatar = ref('')
+
+  const onSubmit = () =>{
+    alert('submit')
+  }
+</script>
